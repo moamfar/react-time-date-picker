06f8c2ddf6c0b16b2fdb83caf38341411304eb31
MohammadrasiM <benkenobl@yahoo.com>
2025-08-02 12:42:02 +0330

    fix: fixed imports in index

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import DatePicker from "./components/DatePicker";
 import TimePicker from "./components/TimePicker";
+import CustomePicker from "./components/CustomePicker";
 export type { DatePickerProps, DatePickerSelectedDate } from "./types/DatePicker.types";
 export type { TimePickerProps, TimePickerSelectedType } from "./types/TimePicker.types";
-export { DatePicker, TimePicker };
+export type { CustomePickerProps, CustomePickerValueType } from "./types/CustomePicker.types";
+export { DatePicker, TimePicker, CustomePicker };
